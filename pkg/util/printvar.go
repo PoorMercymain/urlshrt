b677df895e4542dbc482872dc3ad684790c44b81
PoorMercymain <pinegin17@bk.ru>
2023-09-25 19:39:00 +0300

    goimports

--- pkg/util/printvar.go
+++ pkg/util/printvar.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func PrintVariable(variable string, shortDescription string) {
 	if variable != "" {
-		fmt.Println("Build", shortDescription + ":", variable)
+		fmt.Println("Build", shortDescription+":", variable)
 	} else {
-		fmt.Println("Build", shortDescription + ": N/A")
+		fmt.Println("Build", shortDescription+": N/A")
 	}
 }
