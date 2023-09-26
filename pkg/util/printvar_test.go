5e352d3c99baa6aaac1c4bfebfc5339439157dff
PoorMercymain <pinegin17@bk.ru>
2023-09-26 19:01:23 +0300

    test for print variable updated

--- pkg/util/printvar_test.go
+++ pkg/util/printvar_test.go
@@ -1,10 +1,31 @@
 package util
 
-import "testing"
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestPrintVariable(t *testing.T) {
+	originalStdout := os.Stdout
+	r, w, err := os.Pipe()
+	require.NoError(t, err)
+	os.Stdout = w
+
 	PrintVariable("", "test var")
 	PrintVariable("a", "another test var")
-	// Build test var: N/A
-	// Build another test var: a
+
+	w.Close()
+	os.Stdout = originalStdout
+
+	var buf bytes.Buffer
+	_, err = buf.ReadFrom(r)
+	require.NoError(t, err)
+	
+	got := buf.String()
+	want := "Build test var: N/A\nBuild another test var: a\n"
+
+	require.Equal(t, want, got)
 }
